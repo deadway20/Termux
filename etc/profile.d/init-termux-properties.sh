4c03b835dd5abcdec24dcfc59949c2a7b8cbc224
Coder-X <deadway20@gmail.com>
2021-08-14 17:31:35 +0200

    Delete etc/profile.d directory

--- etc/profile.d/init-termux-properties.sh
+++
@@ -1,4 +0,0 @@
-if [ ! -f /data/data/com.termux/files/home/.config/termux/termux.properties ] && [ ! -e /data/data/com.termux/files/home/.termux/termux.properties ]; then
-mkdir -p /data/data/com.termux/files/home/.termux
-cp /data/data/com.termux/files/usr/share/examples/termux/termux.properties /data/data/com.termux/files/home/.termux/
-fi
