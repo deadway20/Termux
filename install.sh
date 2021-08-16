1035ecd8de1e0a0cce38e8dc21164fe1768ef876
Coder-X <deadway20@gmail.com>
2021-08-16 22:11:37 +0200

    modify install.sh file

--- install.sh
+++ install.sh
@@ -1,11 +1,69 @@
 #!/bin/bash
+
+echo "...installing mc File-Manger..."
+apt install mc -y  &&
+sleep 1
+
+echo "...installing curl..."
+apt install curl -y &&
+sleep 1
+
+echo "...installing git tool..."
+apt install git -y  &&
+sleep 1
+
+echo "...installing python..."
+apt install python -y &&
+sleep 1
+
+echo "...installing python2..."
+apt install python2 -y &&
+sleep 1
+
+echo "...installing nano or update it if exist"
+apt install nano -y &&
+sleep 1
+
+echo "...installing fish Shell"
+apt install fish -y &&
+sleep 1
+
+echo "...installing neofetch..."
+apt install neofetch &&
+sleep 1
+
+echo "...installing lolcat..."
+pip install lolcat &&
+sleep 1
+
+echo "...installing Locate Tool..."
+apt install locate  &&
+sleep 1
+
+echo "...installing openssh..."
+apt install ssh  &&
+sleep 1
+
+echo "...installing figlet..."
+apt install figlet  &&
+sleep 1
+
+echo "...installing tree..."
+apt install tree &&
+sleep 1
+
 echo "...deleting Welcome Message..."
 cd ~ && rm ../usr/etc/motd
-sleep 10000 
+sleep 1
+
 echo "...Restore termux files..."
-mkdir -p ~/.termux &&
-cp -rf ~/advance-key/termux.properties $HOME/.termux/termux.properties &&
-sleep 10000
-echo "...Restore Modifyed bash.bashrc..."
-cp -rf ~/advance-key/bash.bashrc ~/../usr/etc/ 
+mkdir -p ~/ &&
+cp -rf ~/Termux/.termux $HOME/
+sleep 1
+
+cp -rf ~/Termux/etc/* ~/../usr/etc/ 
+sleep 1
+
+cp -rf ~/Termux/.config $HOME/
+sleep 1
 exit
