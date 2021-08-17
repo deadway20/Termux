98a29c984459dfae9e1432579e7d897d5e9cc8ca
Coder-X <deadway20@gmail.com>
2021-08-17 05:17:23 +0200

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,8 @@
 if status is-interactive
 	
-	alias c='clear'
-  alias x='exit'
-	alias h='history'
+  alias c='clear'
+  alias e='exit'
+  alias h='history'
   alias bsh="nano ~/..usr/etc/bash.bashrc"
   alias fsh="nano ~/.config/fish/functions/fish_prompt.fish"
 end
